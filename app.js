f5345a3b0255b5694bc4ceb368a07994ef5147bc
Martin <martinconradpelayo@gmail.com>
2017-03-06 22:07:20 -0800

    Not sure if this is done

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+//Not sure how much is supposed to go here, it wasnt specified in the instructions, hope this works
+function oldProjects(name, link, description){
+    this.name = name;
+    this.link = link;
+    this.description = description;
+}
+var pro1Name = new name('Cookie');
+var pro1link = new link('https://github.com/MartinPelayo/w2audit');
+var pro1descitpion = new description('A cookie Store');
+ 
